5434fec1edfeb7d01fd8190fcb8099f1588ea34e
sianesantoso <142404881+sianesantoso@users.noreply.github.com>
2024-12-28 12:18:05 +0700

    Update streamlit_code.py

--- project_akhir/streamlit_code.py
+++ project_akhir/streamlit_code.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 # Load model yang sudah dibuat sebelumnya 
-with open('best_model.pkl', 'rb') as file:
+with open('project_akhir/best_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # judul
